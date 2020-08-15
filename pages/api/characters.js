a1fcb6665511cf8d2e1bde0fddb4f09115c748d7
Fernando Pinheiro <fnandogp@gmail.com>
2020-08-15 13:42:07 +1000

    add characters endpoint api

---
+++ pages/api/characters.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import md5 from 'js-md5';
+
+export default async (req, res) => {
+  const ts = Math.floor(Date.now() / 1000);
+  const publicKey = process.env.PUBLIC_KEY;
+  const privatecKey = process.env.PRIVATE_KEY;
+  const hash = md5(`${ts}${privatecKey}${publicKey}`);
+
+  const response = await axios.get(
+    'http://gateway.marvel.com/v1/public/characters',
+    { params: { ts, apikey: publicKey, hash } }
+  );
+
+  res.status(200).json({ response: response.data });
+};
